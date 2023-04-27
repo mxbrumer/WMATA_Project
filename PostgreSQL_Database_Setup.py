1f4a3569d831425f6cad8c740562becba867c1da
mxbrumer <mxbrumer@gmail.com>
2023-04-27 12:07:12 -0400

    Add postgreSQL database code

---
+++ PostgreSQL_Database_Setup.py
@@ -0,0 +1,29 @@
+import psycopg2
+
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+#database = config['postgreSQL']['database']
+database = 'test'
+host = config['postgreSQL']['host']
+user = config['postgreSQL']['user']
+password = config['postgreSQL']['password']
+port = config['postgreSQL']['port']
+
+SQLConn = psycopg2.connect(database = database,
+                           host = host,
+                           user = user,
+                           password = password,
+                           port = port)
+
+SQLConn.autocommit = True
+
+
+cursor = SQLConn.cursor()
+
+sql = ''' CREATE DATABASE wmata_data ''';
+
+cursor.execute(sql)
+
+SQLConn.close()
