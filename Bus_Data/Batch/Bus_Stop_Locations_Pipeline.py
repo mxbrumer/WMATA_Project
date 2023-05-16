2cfbef297fb610c79bca6f58c9df035f7857bde8
mxbrumer <mxbrumer@gmail.com>
2023-05-16 11:07:58 -0400

    add gold level code for bus stop locations

--- Bus_Data/Batch/Bus_Stop_Locations_Pipeline.py
+++ Bus_Data/Batch/Bus_Stop_Locations_Pipeline.py
@@ -1,15 +1,24 @@
 from ENV_Setup import *
 from Bus_Stop_Locations_Functions import *
 from Postgres_Functions import *
+from Bus_Schedule_Functions import *
 
-# Create bronze level route tables and save to postgres################################################################################
 APIKey = config['WMATA']['api_key']
 table = 'busstoplocations'
-schema = 'bronze'
+
+# Create bronze level route tables and save to postgres################################################################################
 
 locationsJson = pull_bus_stop_locations_from_api(APIKey, latitude = '', longitude = '', radius = '')
 
 locationsPd = convert_stop_location_json_to_pandas(locationsJson)
 
-save_table_to_postgres(locationsPd, table, schema)
+save_table_to_postgres(locationsPd, table, schema = 'bronze')
 
+# Create gold level route tables and save to postgres################################################################################
+
+locationsPd = retireve_sql_table(schema = "bronze",
+                                 table = table)
+
+locationsPd = convert_text_to_int(locationsPd, 'StopID')
+
+save_table_to_postgres(locationsPd, table, schema = 'gold')
